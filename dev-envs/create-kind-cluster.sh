9cba216fc0ea59c7ede45ab0c32b53872c670041
fbalicchia <fbalicchia@cuebiq.com>
2023-04-04 11:16:21 +0200

    move config map

--- dev-envs/create-kind-cluster.sh
+++ dev-envs/create-kind-cluster.sh
@@ -2,5 +2,6 @@ kind create cluster --config clusterconfig-1.24.1.yaml --name argocd
 kubectl create ns argocd
 kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.7/manifests/install.yaml -n argocd
 kubectl create ns org-147
+kubectl create ns paas-application-staging
 #Check password
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
