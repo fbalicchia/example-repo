228a9a7ef8f605f85f467b6d9ae4119a24ae683c
fbalicchia <fbalicchia@cuebiq.com>
2023-04-04 17:43:22 +0200

    define ingress

--- dev-envs/create-kind-cluster.sh
+++ dev-envs/create-kind-cluster.sh
@@ -1,8 +1,10 @@
 kind create cluster --config clusterconfig-1.24.1.yaml --name argocd
 kubectl create ns argocd
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.7/manifests/install.yaml -n argocd
+#kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.7/manifests/install.yaml -n argocd
+kubectl apply -f install-argocd.yaml -n argocd
 kubectl create ns org-147
 kubectl create ns org-148
 kubectl create ns paas-application-staging
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 #Check password
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
