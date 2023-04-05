319887d8a73efe18c6ac22f21ba5cdf3c8339d36
fbalicchia <fbalicchia@cuebiq.com>
2023-04-05 11:44:48 +0200

    refactoring

--- dev-envs/create-kind-cluster.sh
+++ dev-envs/create-kind-cluster.sh
@@ -1,7 +1,8 @@
-kind create cluster --config clusterconfig-1.24.1.yaml --name argocd
+kind create cluster --config ./conf-files/clusterconfig-1.24.1.yaml --name argocd
 kubectl create ns argocd
 #kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.7/manifests/install.yaml -n argocd
-kubectl apply -f install-argocd.yaml -n argocd
+kubectl apply -f ./conf-files/install-argocd.yaml -n argocd
+echo "####### create ns"
 kubectl create ns org-147
 kubectl create ns org-148
 kubectl create ns paas-application-staging
