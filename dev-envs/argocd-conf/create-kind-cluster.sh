b3e7e5630206e4c31476fa7ea81b8f1aabab7303
fbalicchia <fbalicchia@cuebiq.com>
2023-04-11 17:16:19 +0200

    add nginx value

--- dev-envs/argocd-conf/create-kind-cluster.sh
+++ dev-envs/argocd-conf/create-kind-cluster.sh
@@ -1,7 +1,6 @@
 kind create cluster --config ./clusterconfig-1.24.1.yaml --name argocd
 kubectl create ns argocd
-#kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.7/manifests/install.yaml -n argocd
-kubectl apply -f ./install-argocd.yaml -n argocd
+kubectl apply -f ./argocd-install-latest.yaml -n argocd
 echo "####### CREARTE NAMESPACE #############"
 kubectl create ns org-147
 kubectl create ns org-148
@@ -12,4 +11,7 @@ kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/rele
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 kubectl create serviceaccount -n kubernetes-dashboard admin-user
 kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 #token=$(kubectl -n kubernetes-dashboard create token admin-user)
+
+
