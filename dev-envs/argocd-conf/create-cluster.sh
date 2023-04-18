f017e24481d4216751694ace269ca22147d63ece
fbalicchia <fbalicchia@cuebiq.com>
2023-04-18 14:53:22 +0200

    add org

---
+++ dev-envs/argocd-conf/create-cluster.sh
@@ -0,0 +1,27 @@
+kind delete clusters --all
+cat <<EOF | kind create cluster -n argocd --config -
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  image: kindest/node:v1.24.1
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+kubectl create ns argocd
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/install.yaml -n argocd
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl patch cm/argocd-cm -n argocd --type=merge \
+-p='{"data":{"resource.customizations.health.argoproj.io_Application":"hs = {}\nhs.status = \"Progressing\"\nhs.message = \"\"\nif obj.status ~= nil then\n  if obj.status.health ~= nil then\n    hs.status = obj.status.health.status\n    if obj.status.health.message ~= nil then\n      hs.message = obj.status.health.message\n    end\n  end\nend\nreturn hs\n"}}'
--- dev-envs/argocd-conf/create-kind-cluster.sh
+++ dev-envs/argocd-conf/create-kind-cluster.sh
@@ -1,16 +1,16 @@
 kind create cluster --config ./clusterconfig-1.24.1.yaml --name argocd
 kubectl create ns argocd
-kubectl apply -f ./argocd-install-latest.yaml -n argocd
+kubectl apply -f  -n argocd
 echo "####### CREARTE NAMESPACE #############"
 kubectl create ns org-147
 kubectl create ns org-148
 kubectl create ns argo-rollouts
 kubectl create ns paas-application-staging
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-kubectl create serviceaccount -n kubernetes-dashboard admin-user
-kubectl create clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
+kubectl delete -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl delete serviceaccount -n kubernetes-dashboard admin-user
+kubectl delete clusterrolebinding -n kubernetes-dashboard admin-user --clusterrole cluster-admin --serviceaccount=kubernetes-dashboard:admin-user
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 #token=$(kubectl -n kubernetes-dashboard create token admin-user)
 
